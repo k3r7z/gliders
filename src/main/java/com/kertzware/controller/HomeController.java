d26a326e9c0994f0644ac1a928aaa9b71b2f3b9c
k3r7z <k3r7z@protonmail.com>
2026-01-10 18:26:23 -0300

    Added home controller

---
+++ src/main/java/com/kertzware/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.kertzware.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/*
+    This controller serves a simple "Hello world" message at the /api/hello-world endpoint
+    to test the app works fine. It includes a basic login functionality.
+ */
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "index";
+    }
+}
