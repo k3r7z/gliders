cfc059162b64f4fd8d98568bca97c2531bab7649
k3r7z <k3r7z@protonmail.com>
2026-01-12 00:20:46 -0300

    Last changes on the main index

--- src/main/java/com/kertzware/controller/HomeController.java
+++ src/main/java/com/kertzware/controller/HomeController.java
@@ -15,4 +15,9 @@ public class HomeController {
     public String home(){
         return "index";
     }
+
+    @GetMapping("/contact")
+    public String contact(){
+        return "contact";
+    }
 }
