55fd2fc63b1c59b92eb30d41c26542201e9432ff
k3r7z <k3r7z@protonmail.com>
2026-03-11 13:56:09 -0300

    Lastest work on the flight form. Trying to make a multiple-phased form

--- src/main/resources/static/script/script.js
+++ src/main/resources/static/script/script.js
@@ -1,3 +1,13 @@
-document.getElementById("stopTime").addEventListener("change", event => {
-    console.log("Hora: ", event.target.value);
+document.addEventListener("DOMContentLoaded", () => {
+    const dynamicForm = new bootstrap.Carousel("#dynamicForm");
+    const airplaneBtn = document.getElementById("airplaneBtn");
+    const gliderBtn = document.getElementById("gliderBtn");
+    const aircraftSelect = document.getElementById("aircraftId");
+    const towAirplaneSelect = document.getElementById("towAirplaneId");
+
+    airplaneBtn.addEventListener("click", (event) => {
+        dynamicForm.next();
+    });
+
 });
+
