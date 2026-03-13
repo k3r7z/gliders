49d52dcaa00982b6baddf7ce1c74fb00835c5e4a
k3r7z <k3r7z@protonmail.com>
2026-03-13 09:51:09 -0300

    Added tow section dynamic (dis)appearance depending on the type of aircraft selected

--- src/main/resources/static/script/script.js
+++ src/main/resources/static/script/script.js
@@ -1,13 +1,53 @@
+/**
+ * Constats
+ * @type {string}
+ */
+const PLANE_OPT = "Avión";
+const GLIDER_OPT = "Planeador";
+
+/**
+ * Declare common elements variables
+ */
+let gliderBtn;
+let aircraftSelect;
+let towAirplaneSelect;
+let towAirplaneSection;
+let towSectionCollapse;
+let aircraftTypeInput;
+
+/**
+ * Load the main elements, constants and link events once the DOM is loaded
+ */
 document.addEventListener("DOMContentLoaded", () => {
-    const dynamicForm = new bootstrap.Carousel("#dynamicForm");
-    const airplaneBtn = document.getElementById("airplaneBtn");
-    const gliderBtn = document.getElementById("gliderBtn");
-    const aircraftSelect = document.getElementById("aircraftId");
-    const towAirplaneSelect = document.getElementById("towAirplaneId");
+    gliderBtn = document.getElementById("gliderBtn");
+    aircraftSelect = document.getElementById("aircraftId");
+    towAirplaneSelect = document.getElementById("towAirplaneId");
+    towAirplaneSection = document.getElementById("towAirplaneSection");
+    aircraftTypeInput = document.getElementById("aircraftType");
+    towSectionCollapse = new bootstrap.Collapse(towAirplaneSection, {toggle: false});
 
-    airplaneBtn.addEventListener("click", (event) => {
-        dynamicForm.next();
-    });
-
+    // Start linking event listeners
+    aircraftSelect.addEventListener("change", event => handleAircraftSelection(event));
 });
 
+
+/**
+ * Handles the on change event for the aircraft selection
+ * @param event
+ */
+function handleAircraftSelection(event) {
+    const selectedAircraft = aircraftSelect.options[aircraftSelect.selectedIndex];
+
+    if(selectedAircraft.value !== "") {
+        const type = selectedAircraft.getAttribute('data-type');
+        aircraftTypeInput.innerText = type;
+        if (type === GLIDER_OPT)
+            towSectionCollapse.show();
+        else {
+            towSectionCollapse.hide();
+            towAirplaneSelect.value = "";
+        }
+    }
+    else aircraftTypeInput.innerText = "Tipo de aeronave";
+
+}
