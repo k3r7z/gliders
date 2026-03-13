9adbece6f87ed280de3dafc0e02bbccf9a44d927
k3r7z <k3r7z@protonmail.com>
2026-03-13 10:34:31 -0300

    Added form validation messages

--- src/main/resources/static/script/script.js
+++ src/main/resources/static/script/script.js
@@ -14,11 +14,13 @@ let towAirplaneSelect;
 let towAirplaneSection;
 let towSectionCollapse;
 let aircraftTypeInput;
+let flightForm;
 
 /**
  * Load the main elements, constants and link events once the DOM is loaded
  */
 document.addEventListener("DOMContentLoaded", () => {
+    flightForm = document.getElementById("flightForm");
     gliderBtn = document.getElementById("gliderBtn");
     aircraftSelect = document.getElementById("aircraftId");
     towAirplaneSelect = document.getElementById("towAirplaneId");
@@ -27,9 +29,23 @@ document.addEventListener("DOMContentLoaded", () => {
     towSectionCollapse = new bootstrap.Collapse(towAirplaneSection, {toggle: false});
 
     // Start linking event listeners
+    flightForm.addEventListener("submit", event => validateForm(event));
     aircraftSelect.addEventListener("change", event => handleAircraftSelection(event));
 });
 
+
+
+function validateForm(event) {
+    if (!flightForm.checkValidity()){
+        console.log("Inválido!");
+        event.preventDefault();
+        event.stopPropagation();
+    }
+    else
+        console.log("Válido!");
+
+    flightForm.classList.add("was-validated");
+}
 
 /**
  * Handles the on change event for the aircraft selection
@@ -49,5 +65,4 @@ function handleAircraftSelection(event) {
         }
     }
     else aircraftTypeInput.innerText = "Tipo de aeronave";
-
 }
